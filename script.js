ecc08025977e765694c1997be52337bbdecf0d44
Marian-IT <ionitamarian1998@gmail.com>
2026-07-26 01:09:09 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -414,3 +414,25 @@ window.addEventListener("load", () => {
     }, 5000);
 
 });
+
+const hamburger=document.getElementById("hamburger");
+
+const navMenu=document.getElementById("navMenu");
+
+hamburger.addEventListener("click",()=>{
+
+navMenu.classList.toggle("active");
+
+hamburger.classList.toggle("active");
+
+});
+
+document.querySelectorAll(".nav-menu a").forEach(link=>{
+
+link.addEventListener("click",()=>{
+
+navMenu.classList.remove("active");
+
+});
+
+});
